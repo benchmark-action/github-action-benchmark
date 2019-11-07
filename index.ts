4c71d320bb5b321d030d2adf78cdd897d672ccc7
rhysd <lin90162@yahoo.co.jp>
2019-11-07 18:58:00 +0900

    wip

--- index.ts
+++ index.ts
@@ -1,15 +1,17 @@
 import * as core from '@actions/core';
-import * as github from '@actions/github';
+import { configFromJobInput } from './config';
+import { extractResult } from './extract';
+import { writeBenchmark } from './write';
 
-try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = new Date().toTimeString();
-    core.setOutput('time', time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(`The event payload: ${payload}`);
-} catch (error) {
-    core.setFailed(error.message);
+async function main() {
+    const config = await configFromJobInput();
+    core.debug(`Config extracted from job: ${config}`);
+
+    const bench = await extractResult(config);
+    core.debug(`Benchmark result was extracted: ${bench}`);
+
+    await writeBenchmark(bench, config);
+    core.debug('Benchmark was pushed successfully');
 }
+
+main().catch(e => core.setFailed(e.message));
