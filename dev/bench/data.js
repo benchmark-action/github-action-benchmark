7d0a8b9f55d7547696e2791ac7167dd2e143c1e6
github-action-benchmark <github@users.noreply.github.com>
2019-11-08 07:34:40 +0000

    add cargo benchmark result for [object Object]

---
+++ dev/bench/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1573198480556,
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "lin90162@yahoo.co.jp",
+            "name": "rhysd",
+            "username": "rhysd"
+          },
+          "committer": {
+            "email": "lin90162@yahoo.co.jp",
+            "name": "rhysd",
+            "username": "rhysd"
+          },
+          "distinct": true,
+          "id": "ce98bc6860729bc6f54da88577acef573816227f",
+          "message": "include commit payload",
+          "timestamp": "2019-11-08T16:33:03+09:00",
+          "tree_id": "824ffac5403688b0d8cd58b8d105f11a9dce2b0f",
+          "url": "https://github.com/rhysd/github-action-benchmark/commit/ce98bc6860729bc6f54da88577acef573816227f"
+        },
+        "date": 1573198480535,
+        "benches": [
+          {
+            "name": "bench_fib_10",
+            "value": 255,
+            "range": 32,
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_fib_20",
+            "value": 34058,
+            "range": 3820,
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
