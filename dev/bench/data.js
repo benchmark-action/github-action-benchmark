9dbf34d0d63e6510161392b13e30c2fa16fe9586
github-action-benchmark <github@users.noreply.github.com>
2019-11-08 04:06:39 +0000

    add cargo benchmark result for 639428997441727a2ee9fd17ac7ab68068a59f4a

---
+++ dev/bench/data.js
@@ -0,0 +1,25 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1573185999038,
+  "benches": {
+    "Benchmark": [
+      {
+        "commit": "639428997441727a2ee9fd17ac7ab68068a59f4a",
+        "date": 1573185999021,
+        "benches": [
+          {
+            "name": "bench_fib_10",
+            "value": 246,
+            "range": 19,
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_fib_20",
+            "value": 33497,
+            "range": 1829,
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
