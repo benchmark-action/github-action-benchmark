54076b110b8ddec1b8fac7f893faf608449050fd
github-action-benchmark <github@users.noreply.github.com>
2019-11-08 08:00:39 +0000

    add cargo benchmark result for [object Object]

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1573198480556,
+  "lastUpdate": 1573200039566,
   "entries": {
     "Rust Benchmark": [
       {
@@ -34,6 +34,42 @@ window.BENCHMARK_DATA = {
             "name": "bench_fib_20",
             "value": 34058,
             "range": 3820,
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "lin90162@yahoo.co.jp",
+            "name": "rhysd",
+            "username": "rhysd"
+          },
+          "committer": {
+            "email": "lin90162@yahoo.co.jp",
+            "name": "rhysd",
+            "username": "rhysd"
+          },
+          "distinct": true,
+          "id": "7f28c47ca829b7a19dd7f527aca1375a1cf17b8a",
+          "message": "include tool to benchmark data",
+          "timestamp": "2019-11-08T16:58:16+09:00",
+          "tree_id": "a8250cbb57b38b9a04c6ff1aa708618736ad7591",
+          "url": "https://github.com/rhysd/github-action-benchmark/commit/7f28c47ca829b7a19dd7f527aca1375a1cf17b8a"
+        },
+        "date": 1573200039550,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "bench_fib_10",
+            "value": 241,
+            "range": 34,
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_fib_20",
+            "value": 31572,
+            "range": 4519,
             "unit": "ns/iter"
           }
         ]
