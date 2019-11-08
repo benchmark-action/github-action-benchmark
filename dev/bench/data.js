d7ac19defa49ad761205e9c6dadea15374fdf6f0
github-action-benchmark <github@users.noreply.github.com>
2019-11-08 05:38:00 +0000

    add cargo benchmark result for 6da8c7e7de2dfd277e958b4239b992ac2f2f6d23

---
+++ dev/bench/data.js
@@ -0,0 +1,25 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1573191480137,
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": "6da8c7e7de2dfd277e958b4239b992ac2f2f6d23",
+        "date": 1573191480119,
+        "benches": [
+          {
+            "name": "bench_fib_10",
+            "value": 227,
+            "range": 36,
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_fib_20",
+            "value": 30546,
+            "range": 4484,
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
