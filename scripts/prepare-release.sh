481bacb19c3650c578233300ab3e059bb4d2c9dd
rhysd <lin90162@yahoo.co.jp>
2019-11-10 01:15:17 +0900

    add script to create deploy

---
+++ scripts/prepare-release.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -e
+
+version="$1"
+
+if [[ "$version" == "" ]]; then
+    echo 'Release branch name must be given as first argument' >&2
+    exit 1
+fi
+
+if [ ! -d .git ]; then
+    echo 'This script must be run at root directory of this repository' >&2
+    exit 1
+fi
+
+if ! git diff --quiet; then
+    echo 'Working tree is dirty! Please ensure all changes are commited and working tree is clean' >&2
+    exit 1
+fi
+
+if ! git diff --cached --quiet; then
+    echo 'Git index is dirty! Please ensure all changes are commited and Git index is clean' >&2
+    exit 1
+fi
+
+branch="$(git symbolic-ref --short HEAD)"
+if [[ "$branch" != "master" ]]; then
+    echo 'Current branch is not master. Please move to master before running this script' >&2
+    exit 1
+fi
+
+echo "Releasing to $version branch..."
+
+set -x
+npm install
+npm run build
+npm prune --production
+
+rm -rf .release
+mkdir -p .release
+
+cp action.yml *.js package.json package-lock.json .release/
+cp -R node_modules .release/node_modules
+
+git checkout "$version"
+rm -rf node_modules
+mv .release/action.yml .
+mv .release/*.js .
+mv .release/*.json .
+mv .release/node_modules .
+
+git add action.yml *.js *.json node_modules
+set +x
+
+echo "Done. Please check 'git diff --cached' to verify changes. If ok, add version tag and push it to remote"
