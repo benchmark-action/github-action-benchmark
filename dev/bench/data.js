d5359860168dd9c6ba51124001a291a487d944a9
github-action-benchmark <github@users.noreply.github.com>
2019-11-10 10:16:28 +0000

    add Benchmark.js Benchmark (benchmarkjs) benchmark result for ed511c607d5039ddedfc61128944abda9a5116bb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1573375720618,
+  "lastUpdate": 1573380988541,
   "entries": {
     "Rust Benchmark": [
       {
@@ -1880,6 +1880,42 @@ window.BENCHMARK_DATA = {
             "name": "fib(20)",
             "value": 8519,
             "range": "±0.75%",
+            "unit": "ops/sec"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "lin90162@yahoo.co.jp",
+            "name": "rhysd",
+            "username": "rhysd"
+          },
+          "committer": {
+            "email": "lin90162@yahoo.co.jp",
+            "name": "rhysd",
+            "username": "rhysd"
+          },
+          "distinct": true,
+          "id": "ed511c607d5039ddedfc61128944abda9a5116bb",
+          "message": "add workflow to run unit tests",
+          "timestamp": "2019-11-10T19:15:41+09:00",
+          "tree_id": "b0abe5dbca1f50a5b1870c7e5436481dc9cbda88",
+          "url": "https://github.com/rhysd/github-action-benchmark/commit/ed511c607d5039ddedfc61128944abda9a5116bb"
+        },
+        "date": 1573380988522,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "fib(10)",
+            "value": 999828,
+            "range": "±0.40%",
+            "unit": "ops/sec"
+          },
+          {
+            "name": "fib(20)",
+            "value": 8353,
+            "range": "±0.51%",
             "unit": "ops/sec"
           }
         ]
