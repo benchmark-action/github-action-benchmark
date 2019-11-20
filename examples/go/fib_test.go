000b1573d1634357e61bcb35b99b3c0b1e480413
rhysd <lin90162@yahoo.co.jp>
2019-11-20 20:04:00 +0900

    Revert "temporary: try alert comment"
    This reverts commit 077dde1c236baba9244caad4d9e82ea8399dae20.

--- examples/go/fib_test.go
+++ examples/go/fib_test.go
@@ -6,12 +6,12 @@ import (
 
 func BenchmarkFib10(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		var _ = Fib(13)
+		var _ = Fib(10)
 	}
 }
 
 func BenchmarkFib20(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		var _ = Fib(23)
+		var _ = Fib(20)
 	}
 }
