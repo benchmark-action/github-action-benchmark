b493770aa80237e172eed34091b346ec790e0dfa
github-action-benchmark <github@users.noreply.github.com>
2019-11-21 11:38:16 +0000

    add Go Benchmark (go) benchmark result for 6e9caaf49400b585045206cb045512631c0dd695

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1574336231948,
+  "lastUpdate": 1574336296671,
   "entries": {
     "Rust Benchmark": [
       {
@@ -4371,6 +4371,42 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkFib20",
             "value": 52477,
+            "unit": "ns/op",
+            "extra": "30000 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "lin90162@yahoo.co.jp",
+            "name": "rhysd",
+            "username": "rhysd"
+          },
+          "committer": {
+            "email": "lin90162@yahoo.co.jp",
+            "name": "rhysd",
+            "username": "rhysd"
+          },
+          "distinct": true,
+          "id": "6e9caaf49400b585045206cb045512631c0dd695",
+          "message": "add --no-verify to `git push` to avoid unexpected check",
+          "timestamp": "2019-11-21T20:35:43+09:00",
+          "tree_id": "1d52fc4089aa562ec5b2f31b982b68dc1ec45cb1",
+          "url": "https://github.com/rhysd/github-action-benchmark/commit/6e9caaf49400b585045206cb045512631c0dd695"
+        },
+        "date": 1574336296501,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkFib10",
+            "value": 454,
+            "unit": "ns/op",
+            "extra": "3000000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkFib20",
+            "value": 57051,
             "unit": "ns/op",
             "extra": "30000 times\n2 procs"
           }
