344de897fdac93f1031bce971b09febd029f4002
github-action-benchmark <github@users.noreply.github.com>
2020-01-16 09:33:13 +0000

    add Rust Benchmark (cargo) benchmark result for f0757c8721bfa6e0a557f8414ba206772ca9a1a8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1579167189856,
+  "lastUpdate": 1579167193078,
   "entries": {
     "Rust Benchmark": [
       {
@@ -4822,6 +4822,42 @@ window.BENCHMARK_DATA = {
             "name": "bench_fib_20",
             "value": 34453,
             "range": "+/- 5,966",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "lin90162@yahoo.co.jp",
+            "name": "rhysd",
+            "username": "rhysd"
+          },
+          "committer": {
+            "email": "lin90162@yahoo.co.jp",
+            "name": "rhysd",
+            "username": "rhysd"
+          },
+          "distinct": true,
+          "id": "f0757c8721bfa6e0a557f8414ba206772ca9a1a8",
+          "message": "build action and run Catch2 example locally until new version is released",
+          "timestamp": "2020-01-16T18:31:40+09:00",
+          "tree_id": "be058eb0dbf4ad52c656fbe02e5ec74c3c3967f1",
+          "url": "https://github.com/rhysd/github-action-benchmark/commit/f0757c8721bfa6e0a557f8414ba206772ca9a1a8"
+        },
+        "date": 1579167189789,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "bench_fib_10",
+            "value": 199,
+            "range": "+/- 40",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_fib_20",
+            "value": 27178,
+            "range": "+/- 5,444",
             "unit": "ns/iter"
           }
         ]
