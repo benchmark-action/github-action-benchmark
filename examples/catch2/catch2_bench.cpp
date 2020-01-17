685e085bef015264b067fa703c489af6195b3559
rhysd <lin90162@yahoo.co.jp>
2020-01-17 20:52:32 +0900

    fix benchmarks in catch2 example

--- examples/catch2/catch2_bench.cpp
+++ examples/catch2/catch2_bench.cpp
@@ -5,7 +5,7 @@
 TEST_CASE("Fibonacci") {
 
   // now let's benchmark:
-  BENCHMARK("Fibonacci 20") { return fib(20); };
+  BENCHMARK("Fibonacci 10") { return fib(10); };
 
-  BENCHMARK("Fibonacci 25") { return fib(25); };
+  BENCHMARK("Fibonacci 20") { return fib(20); };
 }
