e66afe35029b0ed13a86da4971c49dc5bb66ee9f
Jason Williams <jase.williams@gmail.com>
2020-01-20 19:25:40 +0000

    * add cargo and target to ignore * add workflow * Make working benchmark

--- examples/criterion/benches/bench.rs
+++ examples/criterion/benches/bench.rs
@@ -1,20 +1,23 @@
-#![feature(test)]
+#[macro_use]
+extern crate criterion;
+use criterion::Criterion;
+use criterion_example::fib;
 
-extern crate test;
-
-use rust_example::fib;
-use test::Bencher;
-
-#[bench]
-fn bench_fib_10(b: &mut Bencher) {
-    b.iter(|| {
-        let _ = fib(10);
+fn bench_fib_10(c: &mut Criterion) {
+    c.bench_function("Bench Fib 10", move |b| {
+        b.iter(|| {
+            let _ = fib(10);
+        });
     });
 }
 
-#[bench]
-fn bench_fib_20(b: &mut Bencher) {
-    b.iter(|| {
-        let _ = fib(20);
+fn bench_fib_20(c: &mut Criterion) {
+    c.bench_function("Bench Fib 20", move |b| {
+        b.iter(|| {
+            let _ = fib(20);
+        });
     });
 }
+
+criterion_group!(benches, bench_fib_10, bench_fib_20);
+criterion_main!(benches);
