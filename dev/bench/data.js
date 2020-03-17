cae671bc7b3c05aab0134accfc455f89dae91c20
github-action-benchmark <github@users.noreply.github.com>
2020-03-17 13:09:56 +0000

    add Benchmark.js Benchmark (benchmarkjs) benchmark result for 7cadfa13e55c8155d15ef0c2c6d0d2b112185dcb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1584450592549,
+  "lastUpdate": 1584450596268,
   "entries": {
     "Rust Benchmark": [
       {
@@ -17491,6 +17491,44 @@ window.BENCHMARK_DATA = {
             "range": "±0.69%",
             "unit": "ops/sec",
             "extra": "87 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "lin90162@yahoo.co.jp",
+            "name": "rhysd",
+            "username": "rhysd"
+          },
+          "committer": {
+            "email": "lin90162@yahoo.co.jp",
+            "name": "rhysd",
+            "username": "rhysd"
+          },
+          "distinct": true,
+          "id": "7cadfa13e55c8155d15ef0c2c6d0d2b112185dcb",
+          "message": "update changelog for v1.8.1",
+          "timestamp": "2020-03-17T22:08:54+09:00",
+          "tree_id": "0303d5cfeafd10c61cc412898828705fda81edc3",
+          "url": "https://github.com/rhysd/github-action-benchmark/commit/7cadfa13e55c8155d15ef0c2c6d0d2b112185dcb"
+        },
+        "date": 1584450592051,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "fib(10)",
+            "value": 1122983,
+            "range": "±1.17%",
+            "unit": "ops/sec",
+            "extra": "85 samples"
+          },
+          {
+            "name": "fib(20)",
+            "value": 9727,
+            "range": "±0.79%",
+            "unit": "ops/sec",
+            "extra": "92 samples"
           }
         ]
       }
