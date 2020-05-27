abb99110b7fa56a16e1e9b0120ff1d36b0be05aa
Kaan C. Fidan <kaancfidan@gmail.com>
2020-05-27 23:32:30 +0300

    Tabled benchmark support added.

--- src/extract.ts
+++ src/extract.ts
@@ -208,7 +208,7 @@ function extractGoResult(output: string): BenchmarkResult[] {
     // Example:
     //   BenchmarkFib20-8           30000             41653 ns/op
     //   BenchmarkDoWithConfigurer1-8            30000000                42.3 ns/op
-    const reExtract = /^(Benchmark\w+)(-\d+)?\s+(\d+)\s+([0-9.]+)\s+(.+)$/;
+    const reExtract = /^(Benchmark\w+\/?.*?)(-\d+)?\s+(\d+)\s+([0-9.]+)\s+(.+)$/;
 
     for (const line of lines) {
         const m = line.match(reExtract);
