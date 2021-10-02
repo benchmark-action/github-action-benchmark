8c570824ac617e814e58c9a0b94b79749aa77168
github-action-benchmark <github@users.noreply.github.com>
2021-10-02 07:26:25 +0000

    add Rust Benchmark (cargo) benchmark result for 92a87597923a9ef11cdb364b7a4603190b0ae889

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1633159419976,
+  "lastUpdate": 1633159585628,
   "entries": {
     "Rust Benchmark": [
       {
@@ -6574,6 +6574,66 @@ window.BENCHMARK_DATA = {
             "name": "bench_fib_20",
             "value": 25857,
             "range": "± 5329",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rhysd@users.noreply.github.com",
+            "name": "Linda_pp",
+            "username": "rhysd"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "92a87597923a9ef11cdb364b7a4603190b0ae889",
+          "message": "Merge pull request #80 from HerringtonDarkholme/patch-1\n\nfix: allow more test name",
+          "timestamp": "2021-10-02T16:22:51+09:00",
+          "tree_id": "dd1785058dc01cee20ca13bfeb3f3bb6c9827eac",
+          "url": "https://github.com/benchmark-action/github-action-benchmark/commit/92a87597923a9ef11cdb364b7a4603190b0ae889"
+        },
+        "date": 1633159583434,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "BenchFib10",
+            "value": 201,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "BenchFib20",
+            "value": 27534,
+            "range": "± 1027",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Fibonacci/Recursive/20",
+            "value": 27120,
+            "range": "± 102",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Fibonacci/Iterative/20",
+            "value": 5,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Fibonacci/Recursive/21",
+            "value": 43911,
+            "range": "± 85",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Fibonacci/Iterative/21",
+            "value": 5,
+            "range": "± 0",
             "unit": "ns/iter"
           }
         ]
