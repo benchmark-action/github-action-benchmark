0ee8a2c062fd890f093b074f05b900132d3336b6
github-action-benchmark <github@users.noreply.github.com>
2021-10-28 20:56:22 +0000

    add Catch2 Benchmark (catch2) benchmark result for 582e655b92b0adfa991e7943884a247e07f6e4e1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1635454568243,
+  "lastUpdate": 1635454582040,
   "entries": {
     "Rust Benchmark": [
       {
@@ -30815,6 +30815,44 @@ window.BENCHMARK_DATA = {
             "name": "Fibonacci 20",
             "value": 22.453,
             "range": "± 3.922",
+            "unit": "us",
+            "extra": "100 samples\n2 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "582e655b92b0adfa991e7943884a247e07f6e4e1",
+          "message": "Bump py from 1.8.0 to 1.10.0 in /examples/pytest (#62)\n\nBumps [py](https://github.com/pytest-dev/py) from 1.8.0 to 1.10.0.\r\n- [Release notes](https://github.com/pytest-dev/py/releases)\r\n- [Changelog](https://github.com/pytest-dev/py/blob/master/CHANGELOG.rst)\r\n- [Commits](https://github.com/pytest-dev/py/compare/1.8.0...1.10.0)\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\n\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2021-10-28T22:55:32+02:00",
+          "tree_id": "7bdc65eab0b166d5d11f0ac2007bdb72e48f50c6",
+          "url": "https://github.com/benchmark-action/github-action-benchmark/commit/582e655b92b0adfa991e7943884a247e07f6e4e1"
+        },
+        "date": 1635454579675,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Fibonacci 10",
+            "value": 197,
+            "range": "± 3",
+            "unit": "ns",
+            "extra": "100 samples\n177 iterations"
+          },
+          {
+            "name": "Fibonacci 20",
+            "value": 27.389,
+            "range": "± 1.882",
             "unit": "us",
             "extra": "100 samples\n2 iterations"
           }
