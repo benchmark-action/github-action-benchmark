c2340026c566d7f9b6c9e566b1fb8f7dd5dd90c9
Chris Trześniewski <k.trzesniewski@gmail.com>
2021-11-21 13:10:28 +0100

    improve linting

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     globals: {
         'ts-jest': {
-            tsconfig: 'tsconfig.spec.json'
+            tsconfig: 'tsconfig.spec.json',
         },
     },
     preset: 'ts-jest',
