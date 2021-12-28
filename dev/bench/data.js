cd12f189c659836640883dac0f54da8ccaea77c7
github-action-benchmark <github@users.noreply.github.com>
2021-12-28 12:21:56 +0000

    add Rust Benchmark (cargo) benchmark result for 558af0c6fa9d6d95b1d1ee6d1d0e7d3978a6943c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1640694113457,
+  "lastUpdate": 1640694116017,
   "entries": {
     "Rust Benchmark": [
       {
@@ -8170,6 +8170,42 @@ window.BENCHMARK_DATA = {
             "name": "Fibonacci/Iterative/21",
             "value": 7,
             "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "razican@protonmail.ch",
+            "name": "Iban Eguia",
+            "username": "Razican"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "558af0c6fa9d6d95b1d1ee6d1d0e7d3978a6943c",
+          "message": "Added option to use Rust benchmark names with spaces (#94)\n\n* Added option to use Rust benchmark names with spaces, useful for Criterion\r\n* Added tests",
+          "timestamp": "2021-12-28T13:21:23+01:00",
+          "tree_id": "aadd2cd41a9668e3179874130934435cb49dfce4",
+          "url": "https://github.com/benchmark-action/github-action-benchmark/commit/558af0c6fa9d6d95b1d1ee6d1d0e7d3978a6943c"
+        },
+        "date": 1640694113491,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "bench_fib_10",
+            "value": 228,
+            "range": "± 2",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_fib_20",
+            "value": 29059,
+            "range": "± 117",
             "unit": "ns/iter"
           }
         ]
