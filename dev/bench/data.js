e93894012c04a726e042dc6963cfaf27bc15f7c0
github-action-benchmark <github@users.noreply.github.com>
2022-01-27 10:40:28 +0000

    add Catch2 Benchmark (catch2) benchmark result for 7e670e22dabf690b4a513048f5926ac3335b2a34

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1643280023053,
+  "lastUpdate": 1643280028003,
   "entries": {
     "Rust Benchmark": [
       {
@@ -34445,6 +34445,44 @@ window.BENCHMARK_DATA = {
             "name": "Fibonacci 20",
             "value": 24.496,
             "range": "± 246",
+            "unit": "us",
+            "extra": "100 samples\n2 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "42@dmathieu.com",
+            "name": "Damien Mathieu",
+            "username": "dmathieu"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7e670e22dabf690b4a513048f5926ac3335b2a34",
+          "message": "Fetch gh-pages branch using github token (#105)\n\n\r\nCo-authored-by: Leonhard Holz <leonhard.holz@googlemail.com>",
+          "timestamp": "2022-01-27T11:39:39+01:00",
+          "tree_id": "1beb09c9c998826071fba1c84438c14b41071055",
+          "url": "https://github.com/benchmark-action/github-action-benchmark/commit/7e670e22dabf690b4a513048f5926ac3335b2a34"
+        },
+        "date": 1643280026252,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Fibonacci 10",
+            "value": 174,
+            "range": "± 20",
+            "unit": "ns",
+            "extra": "100 samples\n177 iterations"
+          },
+          {
+            "name": "Fibonacci 20",
+            "value": 25.474,
+            "range": "± 2.63",
             "unit": "us",
             "extra": "100 samples\n2 iterations"
           }
