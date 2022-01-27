7e670e22dabf690b4a513048f5926ac3335b2a34
Damien Mathieu <42@dmathieu.com>
2022-01-27 11:39:39 +0100

    Fetch gh-pages branch using github token (#105)
    Co-authored-by: Leonhard Holz <leonhard.holz@googlemail.com>

--- test/write.spec.ts
+++ test/write.spec.ts
@@ -53,7 +53,7 @@ class FakedOctokit {
     }
 }
 
-type GitFunc = 'cmd' | 'push' | 'pull';
+type GitFunc = 'cmd' | 'push' | 'pull' | 'fetch';
 class GitSpy {
     history: [GitFunc, unknown[]][];
     pushFailure: null | string;
@@ -129,6 +129,10 @@ jest.mock('../src/git', () => ({
     },
     async pull(...args: unknown[]) {
         gitSpy.call('pull', args);
+        return '';
+    },
+    async fetch(...args: unknown[]) {
+        gitSpy.call('fetch', args);
         return '';
     },
 }));
@@ -904,7 +908,7 @@ describe('writeBenchmark()', function () {
             const autoPush = cfg.autoPush ?? true;
             const skipFetch = cfg.skipFetch ?? false;
             const hist: Array<[GitFunc, unknown[]] | undefined> = [
-                skipFetch ? undefined : ['cmd', ['fetch', 'origin', 'gh-pages:gh-pages']],
+                skipFetch ? undefined : ['fetch', [token, 'gh-pages']],
                 ['cmd', ['switch', 'gh-pages']],
                 fetch ? ['pull', [token, 'gh-pages']] : undefined,
                 ['cmd', ['add', path.join(dir, 'data.js')]],
