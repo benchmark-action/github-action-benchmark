49e7917af78664d481d7c0a711b2b3b52f533e77
github-action-benchmark <github@users.noreply.github.com>
2022-02-17 23:07:23 +0000

    add Catch2 Benchmark (catch2) benchmark result for 4cd86fc1c5d611f9abde220a9e7d4b3fa64aa87e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1645139238767,
+  "lastUpdate": 1645139243271,
   "entries": {
     "Rust Benchmark": [
       {
@@ -36603,6 +36603,44 @@ window.BENCHMARK_DATA = {
             "name": "Fibonacci 20",
             "value": 23.234,
             "range": "± 514",
+            "unit": "us",
+            "extra": "100 samples\n2 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "baronfel@users.noreply.github.com",
+            "name": "Chet Husk",
+            "username": "baronfel"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4cd86fc1c5d611f9abde220a9e7d4b3fa64aa87e",
+          "message": "Update URL to main website from Benchmark.NET README (#111)",
+          "timestamp": "2022-02-18T00:06:38+01:00",
+          "tree_id": "1ddbe434e88cf4cfc7372f8441df294afa0054dd",
+          "url": "https://github.com/benchmark-action/github-action-benchmark/commit/4cd86fc1c5d611f9abde220a9e7d4b3fa64aa87e"
+        },
+        "date": 1645139241042,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Fibonacci 10",
+            "value": 165,
+            "range": "± 9",
+            "unit": "ns",
+            "extra": "100 samples\n179 iterations"
+          },
+          {
+            "name": "Fibonacci 20",
+            "value": 23.238,
+            "range": "± 434",
             "unit": "us",
             "extra": "100 samples\n2 iterations"
           }
