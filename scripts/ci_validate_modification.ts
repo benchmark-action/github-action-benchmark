bdd5e69d4c4c6e6cadc3748f403a1ed78518bfd1
Chris Trzesniewski <k.trzesniewski@gmail.com>
2022-03-27 14:15:48 +0200

    add debug info fo ci_validate_modification.ts * only afterJSON

--- scripts/ci_validate_modification.ts
+++ scripts/ci_validate_modification.ts
@@ -250,10 +250,10 @@ async function main() {
     console.log('Validating diffs:', diffs);
 
     if (!diffs || diffs.length !== 2) {
-        console.log('--- beforeJSON ---');
-        console.log(JSON.stringify(beforeJson));
-        console.log('-----------------');
-        console.log();
+        // console.log('--- beforeJSON ---');
+        // console.log(JSON.stringify(beforeJson));
+        // console.log('-----------------');
+        // console.log();
         console.log('--- afterJSON ---');
         console.log(JSON.stringify(afterJson));
         console.log('-----------------');
