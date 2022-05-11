c4d78db68ba909aa7d89c0c6a62c4479826e6202
github-action-benchmark <github@users.noreply.github.com>
2022-05-11 13:41:46 +0000

    add Go Benchmark (go) benchmark result for 318404ebff79bbdd2746ee919ad02e0f02c881bc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1652276502025,
+  "lastUpdate": 1652276506392,
   "entries": {
     "Rust Benchmark": [
       {
@@ -16623,6 +16623,42 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkFib20",
             "value": 50323,
+            "unit": "ns/op",
+            "extra": "30000 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "318404ebff79bbdd2746ee919ad02e0f02c881bc",
+          "message": "Bump minimist from 1.2.5 to 1.2.6 (#114)\n\nBumps [minimist](https://github.com/substack/minimist) from 1.2.5 to 1.2.6.\r\n- [Release notes](https://github.com/substack/minimist/releases)\r\n- [Commits](https://github.com/substack/minimist/compare/1.2.5...1.2.6)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: minimist\r\n  dependency-type: indirect\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\n\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2022-05-11T15:41:12+02:00",
+          "tree_id": "4b85ce8bf2f12f3ff20ef8073ba6cab298f0896f",
+          "url": "https://github.com/benchmark-action/github-action-benchmark/commit/318404ebff79bbdd2746ee919ad02e0f02c881bc"
+        },
+        "date": 1652276503724,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkFib10",
+            "value": 386,
+            "unit": "ns/op",
+            "extra": "5000000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkFib20",
+            "value": 48044,
             "unit": "ns/op",
             "extra": "30000 times\n2 procs"
           }
