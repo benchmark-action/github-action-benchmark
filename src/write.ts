14060e0ff8a2750feab456320da58a33530819b1
Chris Trzesniewski <k.trzesniewski@gmail.com>
2022-05-19 13:05:00 +0200

    try using --work-dir option

--- src/write.ts
+++ src/write.ts
@@ -389,7 +389,7 @@ async function writeBenchmarkToGitHubPagesWithRetry(
         console.log('GIT CLONE', ghRepository, ghPagesBranch);
         benchmarkBaseDir = './benchmark-data-repository';
         await git.clone(githubToken, ghRepository, benchmarkBaseDir);
-        extraGitArguments = ['-C benchmark-data-repository'];
+        extraGitArguments = ['--work-dir=benchmark-data-repository'];
         await git.checkout(ghPagesBranch, extraGitArguments);
     } else if (!skipFetchGhPages && (!isPrivateRepo || githubToken)) {
         console.log('GIT PULL', ghPagesBranch);
