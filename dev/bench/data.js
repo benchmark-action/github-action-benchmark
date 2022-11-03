a3d5147d246ef8c6e9a0b42998fbe858ac49562a
github-action-benchmark <github@users.noreply.github.com>
2022-11-03 15:11:31 +0000

    add JMH Benchmark (jmh) benchmark result for b2887b657da81fdbbd1b8f8653099da5b246e259

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1667488283651,
+  "lastUpdate": 1667488291068,
   "entries": {
     "Rust Benchmark": [
       {
@@ -40759,6 +40759,38 @@ window.BENCHMARK_DATA = {
             "value": 47.1507064918677,
             "unit": "ns",
             "range": "± 0.010433637757090112"
+          }
+        ]
+      }
+    ],
+    "JMH Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "k.trzesniewski@gmail.com",
+            "name": "Chris Trzesniewski",
+            "username": "ktrz"
+          },
+          "committer": {
+            "email": "k.trzesniewski@gmail.com",
+            "name": "Chris Trzesniewski",
+            "username": "ktrz"
+          },
+          "distinct": true,
+          "id": "b2887b657da81fdbbd1b8f8653099da5b246e259",
+          "message": "Update changelog for v1.15.0",
+          "timestamp": "2022-11-03T16:09:08+01:00",
+          "tree_id": "44376a4cfbda3e5d68c4b22452d70c37ec951616",
+          "url": "https://github.com/benchmark-action/github-action-benchmark/commit/b2887b657da81fdbbd1b8f8653099da5b246e259"
+        },
+        "date": 1667488288025,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "org.openjdk.jmh.samples.JMHSample_01_HelloWorld.wellHelloThere",
+            "value": 1689130414.496377,
+            "unit": "ops/s",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
           }
         ]
       }
