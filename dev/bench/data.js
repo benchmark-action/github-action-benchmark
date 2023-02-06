5e15b43878e1f749836079f16c2472eadc9c27f9
github-action-benchmark <github@users.noreply.github.com>
2023-02-06 08:14:36 +0000

    add Go Benchmark (go) benchmark result for 7eeba8924d4ed651010d72d7319b064fc0c10355

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1675671255376,
+  "lastUpdate": 1675671276496,
   "entries": {
     "Rust Benchmark": [
       {
@@ -18111,6 +18111,42 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkFib20",
             "value": 49907,
+            "unit": "ns/op",
+            "extra": "30000 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7eeba8924d4ed651010d72d7319b064fc0c10355",
+          "message": "Bump json5 from 2.2.0 to 2.2.3 (#148)\n\nBumps [json5](https://github.com/json5/json5) from 2.2.0 to 2.2.3.\r\n- [Release notes](https://github.com/json5/json5/releases)\r\n- [Changelog](https://github.com/json5/json5/blob/main/CHANGELOG.md)\r\n- [Commits](https://github.com/json5/json5/compare/v2.2.0...v2.2.3)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: json5\r\n  dependency-type: indirect\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-02-06T09:13:29+01:00",
+          "tree_id": "dd7b551a82c74b71157a331ca1c23323387b1d18",
+          "url": "https://github.com/benchmark-action/github-action-benchmark/commit/7eeba8924d4ed651010d72d7319b064fc0c10355"
+        },
+        "date": 1675671273380,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkFib10",
+            "value": 407,
+            "unit": "ns/op",
+            "extra": "3000000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkFib20",
+            "value": 49989,
             "unit": "ns/op",
             "extra": "30000 times\n2 procs"
           }
