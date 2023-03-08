aab512dae71503012293f651a45876b5a91231a0
github-action-benchmark <github@users.noreply.github.com>
2023-03-08 20:36:53 +0000

    add Catch2 Benchmark (catch2) benchmark result for 18d5a3a02a05d291c28beca4134d6d02751e6ced

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678307810747,
+  "lastUpdate": 1678307813748,
   "entries": {
     "Rust Benchmark": [
       {
@@ -41677,6 +41677,44 @@ window.BENCHMARK_DATA = {
             "name": "Fibonacci 20",
             "value": 23.203,
             "range": "± 248",
+            "unit": "us",
+            "extra": "100 samples\n2 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "estebanborai@gmail.com",
+            "name": "Esteban Borai",
+            "username": "EstebanBorai"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "18d5a3a02a05d291c28beca4134d6d02751e6ced",
+          "message": "fix: use `commit.id` over `commit` object (#155)",
+          "timestamp": "2023-03-08T21:35:50+01:00",
+          "tree_id": "435bc60b10ab3b66ce15a2acc6799266a116e4d2",
+          "url": "https://github.com/benchmark-action/github-action-benchmark/commit/18d5a3a02a05d291c28beca4134d6d02751e6ced"
+        },
+        "date": 1678307810422,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Fibonacci 10",
+            "value": 185,
+            "range": "± 25",
+            "unit": "ns",
+            "extra": "100 samples\n132 iterations"
+          },
+          {
+            "name": "Fibonacci 20",
+            "value": 29.273,
+            "range": "± 5.444",
             "unit": "us",
             "extra": "100 samples\n2 iterations"
           }
