43d3cce762b7adf013220ee6b2de606dbcb5796d
github-action-benchmark <github@users.noreply.github.com>
2023-03-26 13:07:40 +0000

    add Catch2 Benchmark (catch2) benchmark result for 5112d4a6bcc0377392283d37b648acc987555249

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1679836046645,
+  "lastUpdate": 1679836060185,
   "entries": {
     "Rust Benchmark": [
       {
@@ -42313,6 +42313,44 @@ window.BENCHMARK_DATA = {
             "name": "Fibonacci 20",
             "value": 24.392,
             "range": "± 329",
+            "unit": "us",
+            "extra": "100 samples\n2 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "89384881+matyasberry@users.noreply.github.com",
+            "name": "Matyas B",
+            "username": "matyasberry"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5112d4a6bcc0377392283d37b648acc987555249",
+          "message": "support for JMH parameters (as separate charts) (#161)",
+          "timestamp": "2023-03-26T15:06:49+02:00",
+          "tree_id": "adee13455550e5b530fa67dbb8476d1fbcba0b7c",
+          "url": "https://github.com/benchmark-action/github-action-benchmark/commit/5112d4a6bcc0377392283d37b648acc987555249"
+        },
+        "date": 1679836057416,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Fibonacci 10",
+            "value": 164,
+            "range": "± 2",
+            "unit": "ns",
+            "extra": "100 samples\n182 iterations"
+          },
+          {
+            "name": "Fibonacci 20",
+            "value": 23.23,
+            "range": "± 240",
             "unit": "us",
             "extra": "100 samples\n2 iterations"
           }
