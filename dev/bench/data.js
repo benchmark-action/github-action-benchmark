d23e8e3d5d91aa42386350c366d5bbc72c346f59
github-action-benchmark <github@users.noreply.github.com>
2023-04-26 05:57:39 +0000

    add Catch2 Benchmark (catch2) benchmark result for 87c65e849517e0afeffc99eb5c17e225dd88169a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1682488653972,
+  "lastUpdate": 1682488659163,
   "entries": {
     "Rust Benchmark": [
       {
@@ -42559,6 +42559,44 @@ window.BENCHMARK_DATA = {
             "name": "Fibonacci 20",
             "value": 23.23,
             "range": "± 240",
+            "unit": "us",
+            "extra": "100 samples\n2 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "robertyoungnz@gmail.com",
+            "name": "Robert Young",
+            "username": "robobario"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "87c65e849517e0afeffc99eb5c17e225dd88169a",
+          "message": "feat: enable user to specify the ref being tested (#163)\n\nThe ref will be used preferentially (if specified) when looking up the details\r\nof the commit to record the results against.\r\n\r\nWhy:\r\nWhen used to record results from a performance test run triggered by a\r\ncomment on a PR, the results were recorded against the head commit of main\r\non our branch. Not the tested commit. This change allows users to specify\r\nthe tested ref.\r\n\r\nExample usage:\r\n```\r\nwith:\r\n  ref: refs/pull/${{ github.event.issue.number }}/head\r\n```",
+          "timestamp": "2023-04-26T07:56:46+02:00",
+          "tree_id": "c85336bb4842208afd85660757c345d8ca4ab889",
+          "url": "https://github.com/benchmark-action/github-action-benchmark/commit/87c65e849517e0afeffc99eb5c17e225dd88169a"
+        },
+        "date": 1682488656013,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Fibonacci 10",
+            "value": 209,
+            "range": "± 86",
+            "unit": "ns",
+            "extra": "100 samples\n153 iterations"
+          },
+          {
+            "name": "Fibonacci 20",
+            "value": 27.936,
+            "range": "± 919",
             "unit": "us",
             "extra": "100 samples\n2 iterations"
           }
