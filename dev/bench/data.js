aec8715846580ac0308326d7f708082cf706ee18
github-action-benchmark <github@users.noreply.github.com>
2023-04-26 06:35:03 +0000

    add Benchmark.js Benchmark (benchmarkjs) benchmark result for 990219421ea3837999d49fc6461a3ad83a47502d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1682490898114,
+  "lastUpdate": 1682490903549,
   "entries": {
     "Rust Benchmark": [
       {
@@ -27893,6 +27893,44 @@ window.BENCHMARK_DATA = {
             "name": "fib(20)",
             "value": 9077,
             "range": "±1.05%",
+            "unit": "ops/sec",
+            "extra": "85 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "k.trzesniewski@gmail.com",
+            "name": "Chris Trzesniewski",
+            "username": "ktrz"
+          },
+          "committer": {
+            "email": "k.trzesniewski@gmail.com",
+            "name": "Chris Trzesniewski",
+            "username": "ktrz"
+          },
+          "distinct": true,
+          "id": "990219421ea3837999d49fc6461a3ad83a47502d",
+          "message": "update CHANGELOG.md for v1.17.0",
+          "timestamp": "2023-04-26T08:29:27+02:00",
+          "tree_id": "fa17bfc02e978124f73b6ac1bc51ae752e75beed",
+          "url": "https://github.com/benchmark-action/github-action-benchmark/commit/990219421ea3837999d49fc6461a3ad83a47502d"
+        },
+        "date": 1682490900217,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "fib(10)",
+            "value": 1064675,
+            "range": "±0.69%",
+            "unit": "ops/sec",
+            "extra": "87 samples"
+          },
+          {
+            "name": "fib(20)",
+            "value": 9424,
+            "range": "±0.78%",
             "unit": "ops/sec",
             "extra": "85 samples"
           }
