c68f408dd5333218782821ad25bb299daa43b5c5
github-action-benchmark <github@users.noreply.github.com>
2023-05-06 00:10:20 +0000

    add C++ Benchmark (googlecpp) benchmark result for ee5f49dd203dbbd2557b10683a04112ed7e1e850

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1683331792448,
+  "lastUpdate": 1683331819956,
   "entries": {
     "Rust Benchmark": [
       {
@@ -40331,6 +40331,42 @@ window.BENCHMARK_DATA = {
             "value": 24141.956490236058,
             "unit": "ns/iter",
             "extra": "iterations: 28982\ncpu: 24138.903457318338 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zumix.cpp@gmail.com",
+            "name": "srz_zumix",
+            "username": "srz-zumix"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ee5f49dd203dbbd2557b10683a04112ed7e1e850",
+          "message": "getServerUrl refers to the GITHUB_SERVER_URL environment variable (#169)\n\n* getServerUrl refers to the GITHUB_SERVER_URL environment variable\r\n\r\n* fix indent\r\n\r\n* fix error\r\n\r\n* eslint fix",
+          "timestamp": "2023-05-05T17:09:10-07:00",
+          "tree_id": "3f5aeb9a03b3a75c9ac1c6afbf87288db9f83502",
+          "url": "https://github.com/benchmark-action/github-action-benchmark/commit/ee5f49dd203dbbd2557b10683a04112ed7e1e850"
+        },
+        "date": 1683331817428,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "fib_10",
+            "value": 179.47146811436167,
+            "unit": "ns/iter",
+            "extra": "iterations: 3901004\ncpu: 179.44372781981258 ns\nthreads: 1"
+          },
+          {
+            "name": "fib_20",
+            "value": 24161.71393533227,
+            "unit": "ns/iter",
+            "extra": "iterations: 28948\ncpu: 24158.72253696283 ns\nthreads: 1"
           }
         ]
       }
