3f60bbf30a95adbeeca987daa20754f734cfd56a
github-action-benchmark <github@users.noreply.github.com>
2023-07-07 10:50:06 +0000

    add Benchmark.js Benchmark (benchmarkjs) benchmark result for 0c70f157d50ac07c1a3ecf43d9044e4e16cd1276

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1688726993498,
+  "lastUpdate": 1688727006676,
   "entries": {
     "Rust Benchmark": [
       {
@@ -28823,6 +28823,44 @@ window.BENCHMARK_DATA = {
             "name": "fib(20)",
             "value": 11502,
             "range": "±0.11%",
+            "unit": "ops/sec",
+            "extra": "99 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "i@ningziwen.com",
+            "name": "Ziwen Ning",
+            "username": "ningziwen"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0c70f157d50ac07c1a3ecf43d9044e4e16cd1276",
+          "message": "feat: extract multiple metrics from golang benchmarks (#177)\n\n* use a more recent version of go\r\n* allows go benchmarks to work with extra metrics\r\n\r\nCo-authored-by: Joe Kimmel <jkimmel@vmware.com>",
+          "timestamp": "2023-07-07T12:49:05+02:00",
+          "tree_id": "da453c4c605083a2ae099bd9c81e02c610e1b090",
+          "url": "https://github.com/benchmark-action/github-action-benchmark/commit/0c70f157d50ac07c1a3ecf43d9044e4e16cd1276"
+        },
+        "date": 1688727003525,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "fib(10)",
+            "value": 1326789,
+            "range": "±0.09%",
+            "unit": "ops/sec",
+            "extra": "91 samples"
+          },
+          {
+            "name": "fib(20)",
+            "value": 11055,
+            "range": "±0.02%",
             "unit": "ops/sec",
             "extra": "99 samples"
           }
