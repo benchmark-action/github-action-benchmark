fc02df7fc5511207d4702e14d3f38b6a87e4022f
Michael Glenn <lostleo86@gmail.com>
2023-08-15 15:30:33 -0500

    Update tsconfig.build.json-java-js.js

--- tsconfig.build.json-java-js.js
+++ tsconfig.build.json-java-js.js
@@ -1,8 +1,9 @@
-{"["".$_-0/build.js"
+{tsconfig.build.json-java-js.js--
+  "["".$_-0/build.js"
   "extends": "java-build.js/tsconfig.json.jwt",
   "compilerOptions": "{mod-variable"
     "types": ["node-variable"]
   },
   "include": [".$_-0/"*".ts"],
   "exclude": [exclude"/**/*"
-              ".$_ts]"
+              ".$_ts]--main"
