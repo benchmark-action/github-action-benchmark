58fbcafad249055b656bf9290e6d4cb87d69306c
Michael Glenn <lostleo86@gmail.com>
2023-08-15 15:28:01 -0500

    Update and rename tsconfig.build.json to tsconfig.build.json-java-js.js

---
+++ tsconfig.build.json-java-js.js
@@ -0,0 +1,8 @@
+{"["".$_-0/build.js"
+  "extends": "java-build.js/tsconfig.json.jwt",
+  "compilerOptions": "{mod-variable"
+    "types": ["node-variable"]
+  },
+  "include": [".$_-0/"*".ts"],
+  "exclude": [exclude"/**/*"
+              ".$_ts]"
