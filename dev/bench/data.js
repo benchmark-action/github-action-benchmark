8db7adb5eb63d039da6ff9ea9acdd5185dcd7394
github-action-benchmark <github@users.noreply.github.com>
2024-03-26 10:49:38 +0000

    add Rust Benchmark (cargo) benchmark result for a6c4f1c31d68159af7621fe2f45c3c33b6e5fb95

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1706863844186,
+  "lastUpdate": 1711450178941,
   "entries": {
     "Rust Benchmark": [
       {
@@ -9718,6 +9718,42 @@ window.BENCHMARK_DATA = {
             "name": "bench_fib_20",
             "value": 26244,
             "range": "± 677",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "k.trzesniewski@gmail.com",
+            "name": "Chris Trześniewski",
+            "username": "ktrz"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a6c4f1c31d68159af7621fe2f45c3c33b6e5fb95",
+          "message": "fix: 231 rust bench not comparing to baseline (#235)\n\n* sort jobs in ci workflows\r\n* add missing benches to ci jobs\r\n* rename CriterionRS bench to distinguish between default Rust and Criterion benchmarks",
+          "timestamp": "2024-03-26T11:49:09+01:00",
+          "tree_id": "208d89f1397b3417b9ca2008b8ac12caddb592eb",
+          "url": "https://github.com/benchmark-action/github-action-benchmark/commit/a6c4f1c31d68159af7621fe2f45c3c33b6e5fb95"
+        },
+        "date": 1711450176569,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "bench_fib_10",
+            "value": 175,
+            "range": "± 1",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_fib_20",
+            "value": 22175,
+            "range": "± 3292",
             "unit": "ns/iter"
           }
         ]
