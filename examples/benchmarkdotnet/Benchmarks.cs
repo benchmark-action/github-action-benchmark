b7f1fa1632059d28fcb5e10db14e6f319973d7e0
Chris Trzesniewski <k.trzesniewski@gmail.com>
2024-03-27 11:01:54 +0100

    break other bench

--- examples/benchmarkdotnet/Benchmarks.cs
+++ examples/benchmarkdotnet/Benchmarks.cs
@@ -15,7 +15,7 @@ namespace Sample
         }
 
         [Benchmark]
-        public void Fib10() => Fib(15);
+        public void Fib10() => Fib(20);
 
         [Benchmark]
         public void Fib20() => Fib(20);
