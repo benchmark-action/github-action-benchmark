a1e0375053b07b7d5acfa01d29b0a2066d993283
github-action-benchmark <github@users.noreply.github.com>
2024-05-19 09:45:39 +0000

    add C++ Benchmark (googlecpp) benchmark result for f6ab2e28cdaae1cac6972ef469e21e576ead8643

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1716111937952,
+  "lastUpdate": 1716111939238,
   "entries": {
     "Rust Benchmark": [
       {
@@ -48155,6 +48155,42 @@ window.BENCHMARK_DATA = {
             "value": 19985.4227081483,
             "unit": "ns/iter",
             "extra": "iterations: 34939\ncpu: 19985.094021008044 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "k.trzesniewski@gmail.com",
+            "name": "Chris Trześniewski",
+            "username": "ktrz"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f6ab2e28cdaae1cac6972ef469e21e576ead8643",
+          "message": "fix: Support sub-nanosecond precision on Cargo benchmarks (#246)\n\n* add test to cover new more precise format\r\n* change regexp and parsing of the number to cover sub-nanosecond precision\r\n\r\n---------\r\n\r\nCo-authored-by: blaine-arcjet <146491715+blaine-arcjet@users.noreply.github.com>",
+          "timestamp": "2024-05-19T11:44:47+02:00",
+          "tree_id": "c9e84620bf6d14312882f0af010baf48cb27e663",
+          "url": "https://github.com/benchmark-action/github-action-benchmark/commit/f6ab2e28cdaae1cac6972ef469e21e576ead8643"
+        },
+        "date": 1716111936005,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "fib_10",
+            "value": 158.6229758782208,
+            "unit": "ns/iter",
+            "extra": "iterations: 4409999\ncpu: 158.62123778259362 ns\nthreads: 1"
+          },
+          {
+            "name": "fib_20",
+            "value": 19957.689599086938,
+            "unit": "ns/iter",
+            "extra": "iterations: 35045\ncpu: 19957.682979026966 ns\nthreads: 1"
           }
         ]
       }
