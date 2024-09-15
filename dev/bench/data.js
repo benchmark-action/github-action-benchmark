f6afdd83cd2e7b0fa2f11c7de41a016ae2e61a71
github-action-benchmark <github@users.noreply.github.com>
2024-09-15 18:25:37 +0000

    add C++ Benchmark (googlecpp) benchmark result for c3a5e4ec40b6c935e215555c3440abd1b6891164

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1726424723036,
+  "lastUpdate": 1726424737272,
   "entries": {
     "Rust Benchmark": [
       {
@@ -49971,6 +49971,42 @@ window.BENCHMARK_DATA = {
             "value": 19998.346260545506,
             "unit": "ns/iter",
             "extra": "iterations: 34965\ncpu: 19997.689117689115 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "martin@martincostello.com",
+            "name": "Martin Costello",
+            "username": "martincostello"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c3a5e4ec40b6c935e215555c3440abd1b6891164",
+          "message": "Update C# README (#258)",
+          "timestamp": "2024-09-15T20:24:11+02:00",
+          "tree_id": "ec6bc38243cf0eaf53db94bb42f04b18a32f86a9",
+          "url": "https://github.com/benchmark-action/github-action-benchmark/commit/c3a5e4ec40b6c935e215555c3440abd1b6891164"
+        },
+        "date": 1726424734726,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "fib_10",
+            "value": 159.05753456657752,
+            "unit": "ns/iter",
+            "extra": "iterations: 4391812\ncpu: 159.05300591191065 ns\nthreads: 1"
+          },
+          {
+            "name": "fib_20",
+            "value": 19984.97779933889,
+            "unit": "ns/iter",
+            "extra": "iterations: 35044\ncpu: 19984.408172583044 ns\nthreads: 1"
           }
         ]
       }
