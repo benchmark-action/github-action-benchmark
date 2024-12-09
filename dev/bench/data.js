d959d3a354bee7814caabbce4e65fe0e69faaa81
github-action-benchmark <github@users.noreply.github.com>
2024-12-09 18:53:08 +0000

    add Go Benchmark (go) benchmark result for e3c661617bc6aa55f26ae4457c737a55545a86a4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1733770383297,
+  "lastUpdate": 1733770388601,
   "entries": {
     "Rust Benchmark": [
       {
@@ -25795,6 +25795,60 @@ window.BENCHMARK_DATA = {
             "value": 4,
             "unit": "auxMetricUnits",
             "extra": "30337 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "afonso.tfaria21@gmail.com",
+            "name": "xtrm0",
+            "username": "xtrm0"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e3c661617bc6aa55f26ae4457c737a55545a86a4",
+          "message": "feat: allow to parse generic cargo bench/criterion units (#280)",
+          "timestamp": "2024-12-09T19:51:18+01:00",
+          "tree_id": "005f79e62d174cd1a903941ebbf0a13dadda2946",
+          "url": "https://github.com/benchmark-action/github-action-benchmark/commit/e3c661617bc6aa55f26ae4457c737a55545a86a4"
+        },
+        "date": 1733770384791,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkFib10",
+            "value": 311.4,
+            "unit": "ns/op",
+            "extra": "3871275 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFib20",
+            "value": 39745,
+            "unit": "ns/op",
+            "extra": "30344 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFib20WithAuxMetric",
+            "value": 39740,
+            "unit": "ns/op\t         4.000 auxMetricUnits",
+            "extra": "30376 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFib20WithAuxMetric - ns/op",
+            "value": 39740,
+            "unit": "ns/op",
+            "extra": "30376 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFib20WithAuxMetric - auxMetricUnits",
+            "value": 4,
+            "unit": "auxMetricUnits",
+            "extra": "30376 times\n4 procs"
           }
         ]
       }
