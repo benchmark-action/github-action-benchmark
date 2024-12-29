5f8da3f81e03dbf5395f240ef742aba2f22b1471
Gabriel Garcia <garciat@uber.com>
2024-12-29 13:44:20 +0100

    Implement Deno benchmark extract

--- src/write.ts
+++ src/write.ts
@@ -81,6 +81,8 @@ function biggerIsBetter(tool: ToolType): boolean {
             return false;
         case 'benchmarkdotnet':
             return false;
+        case 'deno':
+            return false;
         case 'customBiggerIsBetter':
             return true;
         case 'customSmallerIsBetter':
