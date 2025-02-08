feb4a0b050cde57d368ae5ea3ab2981187708414
Henrik Ingo <henrik@nyrk.io>
2025-02-08 18:42:25 +0200

    Format the output of changes so it is easier to read

--- src/nyrkio.ts
+++ src/nyrkio.ts
@@ -267,8 +267,8 @@ export async function nyrkioFindChanges(b: Benchmark, config: Config) {
 
     const changes = await postResults(allTestResults, config);
     if (changes && failOnAlert) {
-        core.info('Nyrkiö detected a change in your performance test results. Please see the log for details.');
-        core.info(JSON.stringify(changes));
+        console.error("\n\nNyrkiö detected a change in your performance test results. Please see the log for details.\n");
+        console.error(JSON.stringify(changes, null, 4));
         if (!neverFail) {
             core.setFailed(
                 'Nyrkiö detected a change in your performance test results. Please see the log for details.',
