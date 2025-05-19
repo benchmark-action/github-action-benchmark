e959923a423775bcbc255e10a12f8a4b6f61756c
github-action-benchmark <github@users.noreply.github.com>
2025-05-19 11:34:29 +0000

    add Catch2 Benchmark (catch2) benchmark result for fd128cf83bac95a64b574137ae5165b341b1f58d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747654464496,
+  "lastUpdate": 1747654469187,
   "entries": {
     "Rust Benchmark": [
       {
@@ -56729,6 +56729,44 @@ window.BENCHMARK_DATA = {
             "range": "± 265.284",
             "unit": "us",
             "extra": "100 samples\n2 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "k.trzesniewski@gmail.com",
+            "name": "Chris Trześniewski",
+            "username": "ktrz"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fd128cf83bac95a64b574137ae5165b341b1f58d",
+          "message": "chore: update ubuntu runner from 20.04 to latest (#312)\n\n* update workflow runner for cpp benchmarks to `ubuntu-latest`\n* update google/benchmark and google/googletest versions to work with latest ubuntu version",
+          "timestamp": "2025-05-19T13:33:52+02:00",
+          "tree_id": "dd30fb32bec1317c2b8a772350c65c34959bbf90",
+          "url": "https://github.com/benchmark-action/github-action-benchmark/commit/fd128cf83bac95a64b574137ae5165b341b1f58d"
+        },
+        "date": 1747654465264,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Fibonacci 10",
+            "value": 85.7715,
+            "range": "± 8.81489",
+            "unit": "ns",
+            "extra": "100 samples\n364 iterations"
+          },
+          {
+            "name": "Fibonacci 20",
+            "value": 8.23179,
+            "range": "± 1.04571",
+            "unit": "us",
+            "extra": "100 samples\n4 iterations"
           }
         ]
       }
