38ce1873f73c9c2f4cc5bb09ec26150d922fd558
Chris Trzesniewski <k.trzesniewski@gmail.com>
2025-08-03 12:11:37 +0200

    fix: time units are not normalized
    * normalize ops per time unit

--- src/normalizeUnit.ts
+++ src/normalizeUnit.ts
@@ -2,9 +2,23 @@ export function normalizeUnit(prevUnit: string, currentUnit: string, value: numb
     const prevUnitIndex = TIME_UNITS.indexOf(prevUnit);
     const currentUnitIndex = TIME_UNITS.indexOf(currentUnit);
 
-    const unitDiff = prevUnitIndex - currentUnitIndex;
+    if (prevUnitIndex >= 0 && currentUnitIndex >= 0) {
+        const unitDiff = prevUnitIndex - currentUnitIndex;
 
-    return value * 1000 ** unitDiff;
+        return value * 1000 ** unitDiff;
+    }
+
+    const prevUnitIndex2 = OPS_PER_TIME_UNIT.indexOf(prevUnit);
+    const currentUnitIndex2 = OPS_PER_TIME_UNIT.indexOf(currentUnit);
+
+    if (prevUnitIndex2 >= 0 && currentUnitIndex2 >= 0) {
+        const unitDiff = prevUnitIndex2 - currentUnitIndex2;
+
+        return value * 1000 ** unitDiff;
+    }
+
+    return value;
 }
 
 const TIME_UNITS = ['s', 'ms', 'us', 'ns'];
+const OPS_PER_TIME_UNIT = [...TIME_UNITS].reverse().map((unit) => `ops/${unit}`);
