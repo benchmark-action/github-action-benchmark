4bdcce38c94cec68da58d012ac24b7b1155efe8b
Chris Trzesniewski <k.trzesniewski@gmail.com>
2025-09-06 13:37:15 +0200

    release v1.20.7

--- dist/src/comment/benchmarkCommentTags.js
+++ dist/src/comment/benchmarkCommentTags.js
@@ -1,17 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.wrapBodyWithBenchmarkTags = exports.benchmarkEndTag = exports.benchmarkStartTag = void 0;
+exports.benchmarkStartTag = benchmarkStartTag;
+exports.benchmarkEndTag = benchmarkEndTag;
+exports.wrapBodyWithBenchmarkTags = wrapBodyWithBenchmarkTags;
 const BENCHMARK_COMMENT_TAG = 'github-benchmark-action-comment';
 function benchmarkStartTag(commentId) {
     return `<!-- ${BENCHMARK_COMMENT_TAG}(start): ${commentId} -->`;
 }
-exports.benchmarkStartTag = benchmarkStartTag;
 function benchmarkEndTag(commentId) {
     return `<!-- ${BENCHMARK_COMMENT_TAG}(end): ${commentId} -->`;
 }
-exports.benchmarkEndTag = benchmarkEndTag;
 function wrapBodyWithBenchmarkTags(commentId, body) {
     return `${benchmarkStartTag(commentId)}\n${body}\n${benchmarkEndTag(commentId)}`;
 }
-exports.wrapBodyWithBenchmarkTags = wrapBodyWithBenchmarkTags;
 //# sourceMappingURL=benchmarkCommentTags.js.map
