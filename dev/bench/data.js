76aa8ce2fdee10b0eefafc9a9d5fbcb5703a3cc7
github-action-benchmark <github@users.noreply.github.com>
2026-03-02 21:29:48 +0000

    add Catch2 Benchmark (catch2) benchmark result for 022dc9c1181b2a3b0e15ba786ffc2d97afed0197

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772486973064,
+  "lastUpdate": 1772486988512,
   "entries": {
     "Rust Benchmark": [
       {
@@ -59053,6 +59053,44 @@ window.BENCHMARK_DATA = {
             "name": "Fibonacci 20",
             "value": 8.68762,
             "range": "± 1.42226",
+            "unit": "us",
+            "extra": "100 samples\n4 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "k.trzesniewski@gmail.com",
+            "name": "Chris Trześniewski",
+            "username": "ktrz"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "022dc9c1181b2a3b0e15ba786ffc2d97afed0197",
+          "message": "fix: avoid duplicate package suffix in Go benchmarks [#336]  (#337)\n\n- Fixes duplicate suffixes for users who worked around #264 by including\n  package names in benchmark names\n- Detects when benchmark name already contains package reference (full\n  path, underscored, or ≥2 trailing segments) and skips suffix\n- Adds go-force-package-suffix option to always append suffix regardless\nof suffix detection",
+          "timestamp": "2026-03-02T22:28:54+01:00",
+          "tree_id": "6b2b1de6cb3bd51bbea044b5a412ccb4e48d48de",
+          "url": "https://github.com/benchmark-action/github-action-benchmark/commit/022dc9c1181b2a3b0e15ba786ffc2d97afed0197"
+        },
+        "date": 1772486984980,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Fibonacci 10",
+            "value": 79.9151,
+            "range": "± 3.37409",
+            "unit": "ns",
+            "extra": "100 samples\n361 iterations"
+          },
+          {
+            "name": "Fibonacci 20",
+            "value": 7.5825,
+            "range": "± 586.492",
             "unit": "us",
             "extra": "100 samples\n4 iterations"
           }
