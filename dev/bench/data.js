c3ee8815ad0a5d55f3cff3b3b51eece8b01dea49
github-action-benchmark <github@users.noreply.github.com>
2026-03-02 21:29:29 +0000

    add Go Benchmark (go) benchmark result for 022dc9c1181b2a3b0e15ba786ffc2d97afed0197

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770295881547,
+  "lastUpdate": 1772486969318,
   "entries": {
     "Rust Benchmark": [
       {
@@ -27445,6 +27445,60 @@ window.BENCHMARK_DATA = {
             "value": 4,
             "unit": "auxMetricUnits",
             "extra": "30258 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "k.trzesniewski@gmail.com",
+            "name": "Chris Trześniewski",
+            "username": "ktrz"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "022dc9c1181b2a3b0e15ba786ffc2d97afed0197",
+          "message": "fix: avoid duplicate package suffix in Go benchmarks [#336]  (#337)\n\n- Fixes duplicate suffixes for users who worked around #264 by including\n  package names in benchmark names\n- Detects when benchmark name already contains package reference (full\n  path, underscored, or ≥2 trailing segments) and skips suffix\n- Adds go-force-package-suffix option to always append suffix regardless\nof suffix detection",
+          "timestamp": "2026-03-02T22:28:54+01:00",
+          "tree_id": "6b2b1de6cb3bd51bbea044b5a412ccb4e48d48de",
+          "url": "https://github.com/benchmark-action/github-action-benchmark/commit/022dc9c1181b2a3b0e15ba786ffc2d97afed0197"
+        },
+        "date": 1772486965932,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkFib10",
+            "value": 311.8,
+            "unit": "ns/op",
+            "extra": "3810988 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFib20",
+            "value": 39845,
+            "unit": "ns/op",
+            "extra": "30151 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFib20WithAuxMetric",
+            "value": 39860,
+            "unit": "ns/op\t         4.000 auxMetricUnits",
+            "extra": "30181 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFib20WithAuxMetric - ns/op",
+            "value": 39860,
+            "unit": "ns/op",
+            "extra": "30181 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFib20WithAuxMetric - auxMetricUnits",
+            "value": 4,
+            "unit": "auxMetricUnits",
+            "extra": "30181 times\n4 procs"
           }
         ]
       }
