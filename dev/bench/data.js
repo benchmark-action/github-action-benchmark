319082a9b3edc3018f50ad988a51356dfa0ec38e
github-action-benchmark <github@users.noreply.github.com>
2026-09-15 09:07:28 +0000

    add Catch2 Benchmark (catch2) benchmark result for 64c1e0ae78e8888a8ad9ec376e146ae2b068b01e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789463241887,
+  "lastUpdate": 1789463248758,
   "entries": {
     "Rust Benchmark": [
       {
@@ -63573,6 +63573,44 @@ window.BENCHMARK_DATA = {
             "name": "Fibonacci 20",
             "value": 7.68842,
             "range": "± 618.498",
+            "unit": "us",
+            "extra": "100 samples\n4 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rjhallsted@gmail.com",
+            "name": "RJ Barman",
+            "username": "barbarj"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "64c1e0ae78e8888a8ad9ec376e146ae2b068b01e",
+          "message": "perf: shallow-clone the target branch only (#364)\n\nThis PR modifies the action to shallow clone the target branch only. \n\n**Why**: For large repos, the time to do a full clone and checkout can\nbe 20 minutes plus, which is long enough to cause retry loops when the\nbranch tip is under contention by multiple benchmark runs.\n\n<!-- This is an auto-generated comment: release notes by coderabbit.ai\n-->\n\n## Summary by CodeRabbit\n\n* **Performance Improvements**\n* Benchmark repositories are now retrieved using a faster, shallow clone\nof only the required GitHub Pages branch.\n* Removed the separate branch checkout step during benchmark publishing.\n\n<!-- end of auto-generated comment: release notes by coderabbit.ai -->",
+          "timestamp": "2026-09-15T11:06:51+02:00",
+          "tree_id": "ac3de774909afb22aee51682c9bdf48ac2b7a323",
+          "url": "https://github.com/benchmark-action/github-action-benchmark/commit/64c1e0ae78e8888a8ad9ec376e146ae2b068b01e"
+        },
+        "date": 1789463244470,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Fibonacci 10",
+            "value": 80.2141,
+            "range": "± 4.27478",
+            "unit": "ns",
+            "extra": "100 samples\n364 iterations"
+          },
+          {
+            "name": "Fibonacci 20",
+            "value": 7.64944,
+            "range": "± 424.842",
             "unit": "us",
             "extra": "100 samples\n4 iterations"
           }
