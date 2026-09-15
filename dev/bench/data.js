13f3e0eb9824175f5458dae54c0e235648c289b4
github-action-benchmark <github@users.noreply.github.com>
2026-09-15 09:07:21 +0000

    add Benchmark.js Benchmark (benchmarkjs) benchmark result for 64c1e0ae78e8888a8ad9ec376e146ae2b068b01e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789463236836,
+  "lastUpdate": 1789463241887,
   "entries": {
     "Rust Benchmark": [
       {
@@ -40589,6 +40589,44 @@ window.BENCHMARK_DATA = {
             "range": "±0.66%",
             "unit": "ops/sec",
             "extra": "96 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rjhallsted@gmail.com",
+            "name": "RJ Barman",
+            "username": "barbarj"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "64c1e0ae78e8888a8ad9ec376e146ae2b068b01e",
+          "message": "perf: shallow-clone the target branch only (#364)\n\nThis PR modifies the action to shallow clone the target branch only. \n\n**Why**: For large repos, the time to do a full clone and checkout can\nbe 20 minutes plus, which is long enough to cause retry loops when the\nbranch tip is under contention by multiple benchmark runs.\n\n<!-- This is an auto-generated comment: release notes by coderabbit.ai\n-->\n\n## Summary by CodeRabbit\n\n* **Performance Improvements**\n* Benchmark repositories are now retrieved using a faster, shallow clone\nof only the required GitHub Pages branch.\n* Removed the separate branch checkout step during benchmark publishing.\n\n<!-- end of auto-generated comment: release notes by coderabbit.ai -->",
+          "timestamp": "2026-09-15T11:06:51+02:00",
+          "tree_id": "ac3de774909afb22aee51682c9bdf48ac2b7a323",
+          "url": "https://github.com/benchmark-action/github-action-benchmark/commit/64c1e0ae78e8888a8ad9ec376e146ae2b068b01e"
+        },
+        "date": 1789463237045,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "fib(10)",
+            "value": 1421058,
+            "range": "±0.65%",
+            "unit": "ops/sec",
+            "extra": "94 samples"
+          },
+          {
+            "name": "fib(20)",
+            "value": 11465,
+            "range": "±0.88%",
+            "unit": "ops/sec",
+            "extra": "98 samples"
           }
         ]
       }
