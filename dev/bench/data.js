7fca55213d3c1dc99fec8adb2c6db0648e279cd6
github-action-benchmark <github@users.noreply.github.com>
2026-09-15 09:08:13 +0000

    add C++ Benchmark (googlecpp) benchmark result for 64c1e0ae78e8888a8ad9ec376e146ae2b068b01e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789463290229,
+  "lastUpdate": 1789463293369,
   "entries": {
     "Rust Benchmark": [
       {
@@ -57623,6 +57623,42 @@ window.BENCHMARK_DATA = {
             "value": 22349.375550309556,
             "unit": "ns/iter",
             "extra": "iterations: 31346\ncpu: 22344.849518279843 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rjhallsted@gmail.com",
+            "name": "RJ Barman",
+            "username": "barbarj"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "64c1e0ae78e8888a8ad9ec376e146ae2b068b01e",
+          "message": "perf: shallow-clone the target branch only (#364)\n\nThis PR modifies the action to shallow clone the target branch only. \n\n**Why**: For large repos, the time to do a full clone and checkout can\nbe 20 minutes plus, which is long enough to cause retry loops when the\nbranch tip is under contention by multiple benchmark runs.\n\n<!-- This is an auto-generated comment: release notes by coderabbit.ai\n-->\n\n## Summary by CodeRabbit\n\n* **Performance Improvements**\n* Benchmark repositories are now retrieved using a faster, shallow clone\nof only the required GitHub Pages branch.\n* Removed the separate branch checkout step during benchmark publishing.\n\n<!-- end of auto-generated comment: release notes by coderabbit.ai -->",
+          "timestamp": "2026-09-15T11:06:51+02:00",
+          "tree_id": "ac3de774909afb22aee51682c9bdf48ac2b7a323",
+          "url": "https://github.com/benchmark-action/github-action-benchmark/commit/64c1e0ae78e8888a8ad9ec376e146ae2b068b01e"
+        },
+        "date": 1789463289213,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "fib_10",
+            "value": 177.44049494755612,
+            "unit": "ns/iter",
+            "extra": "iterations: 3938195\ncpu: 177.42893051258255 ns\nthreads: 1"
+          },
+          {
+            "name": "fib_20",
+            "value": 22322.028813288453,
+            "unit": "ns/iter",
+            "extra": "iterations: 31305\ncpu: 22319.68145663633 ns\nthreads: 1"
           }
         ]
       }
